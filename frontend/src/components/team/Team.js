7a6e491d47376d1942694776a42a36871add1743
Mario Lazzari <mario.lazzari@gmail.com>
2025-03-01 18:13:38 +0100

    Done

--- frontend/src/components/team/Team.js
+++ frontend/src/components/team/Team.js
@@ -18,25 +18,25 @@ const Team = () => {
               <div className='teamItemImage'>
                 <img src={team1} alt='' style={{ width: '100%' }} />
               </div>
-              <div className='teamItemTitle'>@Ileana_d</div>
-              <div className='teamItemName'>Ileana Dierdrich</div>
+              <div className='teamItemTitle'>@Rodolfo_PSC</div>
+              <div className='teamItemName'>John Abrams</div>
               <div className='teamItemPosition'>Artist</div>
             </div>
             <div className='teamItem'>
               <div className='teamItemImage'>
                 <img src={team2} alt='' style={{ width: '100%' }} />
               </div>
-              <div className='teamItemTitle'>@el_lobo_loco</div>
-              <div className='teamItemName'>Jerod Pequot</div>
-              <div className='teamItemPosition'>Dev</div>
+              <div className='teamItemTitle'>@Rodolfo_PSC</div>
+              <div className='teamItemName'>John Abrams</div>
+              <div className='teamItemPosition'>Artist</div>
             </div>
             <div className='teamItem'>
               <div className='teamItemImage'>
                 <img src={team3} alt='' style={{ width: '100%' }} />
               </div>
-              <div className='teamItemTitle'>@amoebaaaa</div>
-              <div className='teamItemName'>Alfred Kensington</div>
-              <div className='teamItemPosition'>Community</div>
+              <div className='teamItemTitle'>@Rodolfo_PSC</div>
+              <div className='teamItemName'>John Abrams</div>
+              <div className='teamItemPosition'>Artist</div>
             </div>
           </div>
         </div>
